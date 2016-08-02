e31142ef29359c68ae8b03c71ed734589ca116a2
heng zhang <heng zhang>
2016-08-02 20:30:26 +0800

    提交文件测试

---
+++ HandParent/core/src/main/java/hand/core/demo/mapper/DemoMapper.java
@@ -0,0 +1,10 @@
+package hand.core.demo.mapper;
+
+import com.hand.hap.mybatis.common.Mapper;
+import hand.core.demo.dto.Demo;
+
+/**
+ * Created by hailor on 16/6/2.
+ */
+public interface DemoMapper extends Mapper<Demo> {
+}
